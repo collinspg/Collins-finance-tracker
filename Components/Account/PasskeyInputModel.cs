4bd209b3e3d99cb303dcb9b62a178d27bda7a339
Chibuike Collins Okolie <collinsokolie90@gmail.com>
2026-07-16 23:56:48 +0100

    Scaffold Blazor Web App with Individual Accounts authentication

---
+++ Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,7 @@
+namespace FinanceTracker.Components.Account;
+
+public class PasskeyInputModel
+{
+    public string? CredentialJson { get; set; }
+    public string? Error { get; set; }
+}
---
+++ Components/Account/PasskeyOperation.cs
@@ -0,0 +1,7 @@
+namespace FinanceTracker.Components.Account;
+
+public enum PasskeyOperation
+{
+    Create = 0,
+    Request = 1,
+}
