a7b33aa406a81b1446852b43e731f34da67ca5b6
Chibuike Collins Okolie <collinsokolie90@gmail.com>
2026-08-12 10:19:42 +0100

    Add CSV export feature for transaction history

---
+++ wwwroot/js/site.js
@@ -0,0 +1,8 @@
+window.downloadFileFromBase64 = (fileName, base64) => {
+    const link = document.createElement('a');
+    link.href = `data:text/csv;base64,${base64}`;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
